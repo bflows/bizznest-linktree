91714cefd3cda835aaea478df00913bec21208b7
bflows <billylflowers@gmail.com>
2026-06-03 18:09:09 -0700

    Add aria-label to ToggleStatsBtn component

--- src/components/ToggleStatsBtn.tsx
+++ src/components/ToggleStatsBtn.tsx
@@ -5,6 +5,7 @@ export default function ToggleStatsBtn({ onToggle, showStats }: { onToggle: () =
     <button
       type="button"
       onClick={onToggle}
+      aria-label="Toggle stats"
       className={`absolute top-5 right-6 p-2 rounded-full w-fit ring-2 cursor-pointer group transition-all duration-300 ease-in-out ${showStats ? "bg-brand800 ring-primary" : "bg-brand800 ring-brand600"} focus:outline-none focus:border-none focus:ring-primary focus:ring-offset-4 focus:ring-offset-brand950`}
     >
       <ChartLine className={`size-5.25 transition-colors duration-300 ease-in-out ${showStats ? "text-primary" : "text-brand400 group-hover:text-brand200 group-focus:text-brand200"}`} />
